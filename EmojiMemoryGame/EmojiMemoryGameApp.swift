21d9e2bcd8d0613671e9c6138a00ce2861d2dcbe
dhkim <dh5437@naver.com>
2025-02-28 22:25:14 +0900

    First Commit

--- EmojiMemoryGame/EmojiMemoryGameApp.swift
+++ EmojiMemoryGame/EmojiMemoryGameApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct EmojiMemoryGameApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EmojiMemoryGameView()
         }
     }
 }
